8d67fcc6ec0a6e369dfd70977fff21b831b003f8
lyk <1285281632@qq.com>
2022-07-27 10:03:17 +0800

    注释

--- testdemo1/src/main/java/com/lyk/demo/vo/res/StudentResVO.java
+++ testdemo1/src/main/java/com/lyk/demo/vo/res/StudentResVO.java
@@ -1,6 +1,7 @@
 package com.lyk.demo.vo.res;
 
 import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 /**
@@ -10,11 +11,16 @@ import lombok.Data;
 @ApiModel(value = "响应前端学生类",description = "响应前端学生信息")
 public class StudentResVO {
 
+    @ApiModelProperty(value = "姓名", example = "张三")
     private String name;
 
+    @ApiModelProperty(value = "班级", example = "九年1班")
     private String classes;
 
+    @ApiModelProperty(value = "性别", example = "F")
     private String gender;
 
+    @ApiModelProperty(value = "年龄", example = "28")
     private Integer age;
+
 }
