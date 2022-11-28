25a23795a6724ac36f7a1555a4771b6d3144634c
lvyikang <lvyikang@wondertek.com.cn>
2022-11-28 10:44:41 +0800

    提交人：lyk 提交内容：包名修改 提交时间：2022-11-28

--- testdemo/src/main/java/DemoApplication.java
+++ testdemo/src/main/java/DemoApplication.java
@@ -1,10 +1,11 @@
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootApplication/*(exclude={DataSourceAutoConfiguration.class})*/
 @ComponentScan(basePackages = {"com.lyk.demo.*"})
 //自动将包下接口生成实现
 @MapperScan(basePackages = {"com.lyk.demo.mapper"})
