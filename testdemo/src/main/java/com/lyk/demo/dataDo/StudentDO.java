048b28bab7331f3c8932cb8a8615e8905b8a6019
lvyikang <lvyikang@wondertek.com.cn>
2022-11-28 13:36:38 +0800

    提交人：lyk 提交内容：测试 提交时间：2022-11-28

--- testdemo/src/main/java/com/lyk/demo/dataDo/StudentDO.java
+++ testdemo/src/main/java/com/lyk/demo/dataDo/StudentDO.java
@@ -2,15 +2,33 @@ package com.lyk.demo.dataDo;
 
 import com.baomidou.mybatisplus.annotation.TableName;
 
+import java.util.Date;
+
 @TableName(value = "student")
 public class StudentDO {
+    private Long id;
+
     private String name;
 
     private String classes;
 
     private String gender;
 
-    private Integer age;
+    private Short age;
+
+    private String school;
+
+    private Date createTime;
+
+    private Date updateTime;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public String getName() {
         return name;
@@ -36,11 +54,35 @@ public class StudentDO {
         this.gender = gender == null ? null : gender.trim();
     }
 
-    public Integer getAge() {
+    public Short getAge() {
         return age;
     }
 
-    public void setAge(Integer age) {
+    public void setAge(Short age) {
         this.age = age;
+    }
+
+    public String getSchool() {
+        return school;
+    }
+
+    public void setSchool(String school) {
+        this.school = school == null ? null : school.trim();
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
     }
 }
--- testdemo/src/main/java/com/lyk/demo/vo/res/StudentResVO.java
+++ testdemo/src/main/java/com/lyk/demo/vo/res/StudentResVO.java
@@ -4,12 +4,17 @@ import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
+import java.util.Date;
+
 /**
  * 学生信息前端响应类
  */
 @Data
 @ApiModel(value = "响应前端学生类",description = "响应前端学生信息")
 public class StudentResVO {
+
+    @ApiModelProperty(value = "学生表主键", example = "111")
+    private Long id;
 
     @ApiModelProperty(value = "姓名", example = "张三")
     private String name;
@@ -22,5 +27,14 @@ public class StudentResVO {
 
     @ApiModelProperty(value = "年龄", example = "28")
     private Integer age;
+
+    @ApiModelProperty(value = "学校", example = "一中")
+    private String school;
+
+    @ApiModelProperty(value = "创建时间")
+    private Date createTime;
+
+    @ApiModelProperty(value = "修改时间")
+    private Date updateTime;
 
 }
